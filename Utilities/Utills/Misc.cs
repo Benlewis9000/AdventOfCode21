f58c670ef78e3ede820adbca3cc513be3fc9a5bd
Ben Lewis <ben.lewis@towerswatson.com>
2021-12-07 09:07:14 +0000

    ConvertStringsToInts can take a base.

--- Utilities/Utills/Misc.cs
+++ Utilities/Utills/Misc.cs
@@ -8,9 +8,9 @@ namespace Shared.Utills
 {
   public class Misc
   {
-    public static List<int> ConvertStringsToInts(IEnumerable<string> strings)
+    public static List<int> ConvertStringsToInts(IEnumerable<string> strings, int bse = 10)
     {
-      return strings.Select(str => str.ToInt()).ToList();
+      return strings.Select(str => Convert.ToInt32(str, bse)).ToList();
     }
   }
 }
